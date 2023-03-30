551f32fe2d02c965519b903d0282dbff80b78b04
SeltikeS <pschenka@gmail.com>
2023-03-30 22:26:45 +0300

    Create Home component

---
+++ src/components/Home/Data.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Data = () => {
+  return (
+    <div className="home__data">
+      <h1 className="home__title">
+        Maxim Gorelikov
+      </h1>
+      <h3 className="home__subtitle">Frontend Developer</h3>
+      <p className="home__description">
+        Hello. I'm creative Frontend Developer who really like his work.
+      </p>
+
+      <a href="#contact" className="button button--flex">
+        Say Hello <i className="uil uil-telegram-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Data;
