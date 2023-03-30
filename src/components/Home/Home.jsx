08e207d5bad831329a3b4387b23ce3eb85cd6eef
SeltikeS <pschenka@gmail.com>
2023-03-30 23:08:08 +0300

    Create ScrollDown component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import './home.css';
 import Social from "./Social";
 import Data from "./Data";
+import ScrollDown from "./ScrollDown";
 
 const Home = () => {
   return (
     <section className="home section" id="home">
-      <div className="home__container container">
+      <div className="home__container container grid">
         <div className="home__content grid">
           <Social />
 
@@ -16,6 +17,8 @@ const Home = () => {
 
           <Data />
         </div>
+
+        <ScrollDown />
       </div>
     </section>
   );
